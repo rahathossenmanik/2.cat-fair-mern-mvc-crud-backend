95a6a9c9ff24af038b2ea536ae888b5ab70b5ea9
Manik Hosen <rahathossenmanik@gmail.com>
2023-02-12 23:49:03 +0600

    ci: code simplified

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express');
+const mongoose = require('mongoose');
 
-const authors = require('./routes/authors')
-const books = require('./routes/books')
+const authors = require('./routes/authors');
+const books = require('./routes/books');
 
-const port = 5000
-const mongo_uri = 'mongodb://localhost:27017/mern-mvc-crud'
+const port = 5000;
+const mongo_uri = 'mongodb://localhost:27017/mern-mvc-crud';
+const app = express();
 
-mongoose.connect(mongo_uri, { useNewUrlParser: true, useFindAndModify: false })
-  .then(() => {
-    console.log(`Conectado a la base de datos: ${mongo_uri}`)
-  })
-  .catch(err => {
-    console.log(err)
-    console.log('Terminando la aplicación ahora...')
-    process.exit()
-  })
+try {
+  mongoose.connect(mongo_uri, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  });
+  console.log(`Connected to Database: ${mongo_uri}`);
+} catch (error) {
+  console.log(`Database Connection Error: ${err}`);
+  process.exit();
+}
 
-const app = express()
-
-app.use(express.json())
-app.use('/api/authors', authors)
-app.use('/api/books', books)
+app.use(express.json());
+app.use('/api/authors', authors);
+app.use('/api/books', books);
 
 app.listen({ port }, () => {
-  console.log(`Servidor listo en http://localhost:${port}`)
-})
+  console.log(`Server is Running on: http://localhost:${port}`);
+});
